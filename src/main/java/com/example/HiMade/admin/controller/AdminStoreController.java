b9f1932972efd62f0b5b4d9ea77bbb604a035201
wjin01 <dnwls0787@gmail.com>
2024-10-22 17:03:39 +0900

    유저메인, 마스터 스토어값 가져오기

--- src/main/java/com/example/HiMade/admin/controller/AdminStoreController.java
+++ src/main/java/com/example/HiMade/admin/controller/AdminStoreController.java
@@ -2,20 +2,14 @@ package com.example.HiMade.admin.controller;
 
 import com.example.HiMade.admin.dto.*;
 import com.example.HiMade.admin.service.AdminStoreService;
-import com.example.HiMade.master.entity.Store;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpSession;
 import java.io.File;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
--- src/main/java/com/example/HiMade/master/repository/StoreRepository.java
+++ src/main/java/com/example/HiMade/master/repository/StoreRepository.java
@@ -1,6 +1,6 @@
 package com.example.HiMade.master.repository;
 
-import com.example.HiMade.master.entity.Store;
+import com.example.HiMade.master.entity.StoreAdmin;
 import com.example.HiMade.master.entity.StoreStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -10,12 +10,12 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
-public interface StoreRepository extends JpaRepository<Store, String> {
+public interface StoreRepository extends JpaRepository<StoreAdmin, Long> {
     @Modifying
     @Transactional
-    @Query("UPDATE Store s SET s.storeStatus = :storeStatus WHERE s.storeNo = :storeNo") //
+    @Query("UPDATE StoreAdmin s SET s.storeStatus = :storeStatus WHERE s.storeNo = :storeNo") //
     void updateStoreStatusByStoreNo(@Param("storeNo") Long storeNo, @Param("storeStatus") StoreStatus storeStatus);
 
-    @Query("SELECT s FROM Store s WHERE s.storeNo = :storeNo")
-    Optional<Store> findByStoreNo(@Param("storeNo") Long storeNo);
+    @Query("SELECT s FROM StoreAdmin s WHERE s.storeNo = :storeNo")
+    Optional<StoreAdmin> findByStoreNo(@Param("storeNo") Long storeNo);
 }

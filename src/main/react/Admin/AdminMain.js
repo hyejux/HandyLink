f92f3a569a529c795823618e9db0a2823e9e15c9
syoung-git <al2440buss@gmail.com>
2024-11-05 18:26:42 +0900

    jsy

---
+++ src/main/react/Admin/AdminMain.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+
+function AdminMain() {
+
+
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <AdminMain />
+);
